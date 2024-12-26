4bdfae892742449ab074b841e6dd81878d2d28ce
Abhishek K K <abhishekkarunakaran100@gmail.com>
2024-12-27 01:35:21 +0530

    added testcases for GetContent

--- src/core/service/pbin_service_test.go
+++ src/core/service/pbin_service_test.go
@@ -17,21 +17,18 @@ func TestSaveContent(t *testing.T) {
 
 	testCases := []struct {
 		name        string
-		payload     *domain.Payload
 		mockErr     error
 		expectedErr error
 		wantErr     bool
 	}{
 		{
 			name:        "#1 Save Content-Postive Test",
-			payload:     &domain.Payload{},
 			mockErr:     nil,
 			expectedErr: nil,
 			wantErr:     false,
 		},
 		{
-			name: "#2 Save Content-Negative Test",
-			payload: &domain.Payload{},
+			name:        "#2 Save Content-Negative Test",
 			mockErr:     errors.New("Some error"),
 			expectedErr: ErrSaveData,
 			wantErr:     true,
@@ -45,11 +42,76 @@ func TestSaveContent(t *testing.T) {
 	for _, testCase := range testCases {
 		mockRepo.EXPECT().AddData(ctx, gomock.Any(), gomock.Any()).Return(testCase.mockErr)
 
-		_, err := pbinService.SaveContent(ctx, testCase.payload)
+		_, err := pbinService.SaveContent(ctx, &domain.Payload{})
+
 		if testCase.wantErr {
 			assert.ErrorIs(t, err, testCase.expectedErr)
+		} else {
+			assert.NoError(t, err)
 		}
+	}
+}
 
+func TestGetContent(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish() // Ensure all expectations are met
+	mockRepo := mock_ports.NewMockRepository(ctrl)
+
+	testCases := []struct {
+		name        string
+		payload     *domain.DataRequest
+		mockData    *domain.Data
+		mockErr     error
+		expectedErr error
+		wantErr     bool
+	}{
+		{
+			name:        "#1 Get Content- Negative Test (Repo Error)",
+			payload:     &domain.DataRequest{Password: "password"},
+			mockData:    nil,
+			mockErr:     errors.New("Some Error"),
+			expectedErr: ErrGetData,
+			wantErr:     true,
+		},
+		{
+			name:        "#2 Get Content- Negative Test (Missing Data)",
+			payload:     &domain.DataRequest{Password: "password"},
+			mockData:    &domain.Data{Password: "", Content: "nonce" + "ciphertext"},
+			mockErr:     nil,
+			expectedErr: ErrGetDataAbsent,
+			wantErr:     true,
+		},
+		{
+			name:        "#3 Get Content- Negative Test (Incorrect Password)",
+			payload:     &domain.DataRequest{Password: "wrong_password"},
+			mockData:    &domain.Data{Password: "$2y$10$...hashed_password...", Content: "nonce" + "ciphertext"},
+			mockErr:     nil,
+			expectedErr: ErrIncorrectPassword,
+			wantErr:     true,
+		},
 	}
 
+	pbinService := NewPbinService(mockRepo)
+
+	ctx := context.Background()
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			if testCase.mockData != nil {
+				mockRepo.EXPECT().GetData(gomock.Any(), gomock.Any()).Return(testCase.mockData, testCase.mockErr)
+			} else {
+				mockRepo.EXPECT().GetData(gomock.Any(), gomock.Any()).Return(nil, testCase.mockErr)
+			}
+
+			content, err := pbinService.GetContent(ctx, testCase.payload)
+
+			if testCase.wantErr {
+				assert.ErrorIs(t, err, testCase.expectedErr)
+				assert.Nil(t, content) 
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, content)
+			}
+		})
+	}
 }
